e158d6db5a63a03d63aa97cdb8d9b4a448ab5457
bartlomiej-gora <bartlomiej.gora@gmail.com>
2016-06-26 18:45:13 +0200

    lesson01: Hello world.

---
+++ lesson01/src/swing/in/miutes/Main.java
@@ -0,0 +1,16 @@
+package swing.in.miutes;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame hello = new HelloFrame();
+                hello.setVisible(true);
+            }
+        });
+    }
+}
