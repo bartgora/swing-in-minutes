ca94721e8066ff806309d0fdc7bedc4609d849b5
bartlomiej-gora <bartlomiej.gora@gmail.com>
2016-06-26 19:10:22 +0200

    lesson02: Hello world, and button

--- lesson02/src/swing/in/minutes/lesson02/HelloFrame.java
+++ lesson02/src/swing/in/minutes/lesson02/HelloFrame.java
@@ -1,7 +1,6 @@
 package swing.in.minutes.lesson02;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,7 +8,8 @@ import java.awt.event.ActionListener;
 public class HelloFrame extends JFrame {
 
     private JButton button;
-    public HelloFrame(){
+
+    public HelloFrame() {
         button = new JButton("Close");
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(new Dimension(400, 200));
