6a7f07657ea8a22cdf0e05dc4fba67eacb7f41b3
bartlomiej-gora <bartlomiej.gora@gmail.com>
2019-03-09 16:44:08 +0100

    lesson04: added menu

--- lesson02/src/swing/in/minutes/lesson02/HelloFrame.java
+++ lesson02/src/swing/in/minutes/lesson02/HelloFrame.java
@@ -1,7 +1,12 @@
 package swing.in.minutes.lesson02;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,10 +19,13 @@ public class HelloFrame extends JFrame {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setSize(new Dimension(400, 200));
         setResizable(false);
+        setLocationRelativeTo(null);
         JPanel center = new JPanel();
         center.add(new JLabel("Hello World"), BorderLayout.CENTER);
         add(center);
-        add(button, BorderLayout.SOUTH);
+        JPanel bottom = new JPanel();
+        bottom.add(button);
+        add(bottom, BorderLayout.SOUTH);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
